2e0347bd6c700d1ca322f9238b144c7b27b938c8
max <kalmro4@gmail.com>
2024-05-03 23:39:57 +0700

    console app connect db

---
+++ ConApp/Models/TSOLSConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ConApp.Models
+{
+    public partial class TSOLSConfig
+    {
+        public string FTCnfCode { get; set; }
+        public string FTCnfName { get; set; }
+        public int FNCnfType { get; set; }
+    }
+}
