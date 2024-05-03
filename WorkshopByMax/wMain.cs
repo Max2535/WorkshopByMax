e2341d8047ed4f713849af145c3ff52b0cb75322
max <kalmro4@gmail.com>
2024-05-03 14:24:06 +0700

    init form

---
+++ WorkshopByMax/wMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkshopByMax
+{
+    public partial class wMain : Form
+    {
+        public wMain()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ WorkshopByMax/wOrders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkshopByMax
+{
+    public partial class wOrders : Form
+    {
+        public wOrders()
+        {
+            InitializeComponent();
+        }
+    }
+}
